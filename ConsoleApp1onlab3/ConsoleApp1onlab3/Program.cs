8fabdf4b16745275258fba1f99c98abc3432bca3
Yordanos-Samson <165791634+Yordanos-Samson@users.noreply.github.com>
2024-06-04 20:05:25 +0300

    Add files via upload

---
+++ ConsoleApp1onlab3/ConsoleApp1onlab3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1onlab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wellcome to program calculator");
+            Program i= new Program();
+            i.input();
+            i.sum();
+         i.product();
+         i.division();
+         i.subtrsact();
+         i.display();
+            
+        }
+        public void input()
+        {
+            double z, x, c;
+            Console.WriteLine("please enter the numbers");
+            z=Convert.ToDouble(Console.ReadLine());
+            x=Convert.ToDouble(Console.ReadLine());
+            c=Convert.ToDouble(Console.ReadLine());
+        }
+        public void sum()
+        {
+
+            double sum = i
+        }
+    }
+}
